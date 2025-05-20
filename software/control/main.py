bf09a3e2cc47a93a185332814bf417af6c7bbf9b
Zak <57948993+zak-2213@users.noreply.github.com>
2025-05-20 21:44:38 +0200

    reorganisation

---
+++ software/control/main.py
@@ -0,0 +1,23 @@
+from gpiozero import Buzzer
+from time import sleep
+from DOA import get_doa_angle
+from basic_movement import forward, turn
+
+bz = Buzzer(11)
+
+if __name__ == "__main__":
+    bz.on()
+    bz.beep()
+    sleep(0.5)
+    bz.off()
+
+    try:
+        while True:
+            doa = get_doa_angle()
+            if doa == -1:
+                break
+            elif doa is not None:
+                turn(doa)
+                forward(5)
+    except KeyboardInterrupt:
+        print("\nProgram terminated cleanly.")
